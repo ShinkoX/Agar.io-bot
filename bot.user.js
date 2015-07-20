4ed56ecb37f1001e8fc8e7b43c0a68bf2a071144
ShinkoX <szgn-uzn@outlook.com>
2015-07-20 14:06:05 +0200

    Update bot.user.js

--- bot.user.js
+++ bot.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name        AposBot
 // @namespace   AposBot
-// @include     http://agar.io/*
+// @include     http://agarp.co/*
 // @version     3.551
 // @grant       none
 // @author      http://www.twitch.tv/apostolique
